1a5e0a7bfc376c441cc0dda473aa41add20333fc
Achrafgasmi58 <achraf.gasmi@esprit.tn>
2022-12-28 14:50:38 +0100

    Home Component design

---
+++ src/components/Home/index.js
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+import LogoTitle from "../../assets/images/blue_logo.png";
+import "./index.scss";
+
+const Home = () => {
+    return(
+        <div className="container home-page">
+            <div className="text-zone">
+                <h1>Hi, <br /> I'm
+                <img src={LogoTitle} alt="developer" />
+                uesmi Achraf
+                <br />
+                Software Engineer
+                </h1>
+                <h2>Full Stack Developer / Digital Marketer / Musician</h2>
+                <Link to ="/contact" className="flat-button">CONTACT ME</Link>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
