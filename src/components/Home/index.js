ea37777909188ca3aa804386caf3fd2e8bf86c76
Achrafgasmi58 <achraf.gasmi@esprit.tn>
2022-12-28 21:17:43 +0100

    packman loader done, contact form done but no email js

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import { Link } from "react-router-dom";
+import Loader from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters'
 import LogoTitle from "../../assets/images/blue_logo.png";
 import "./index.scss";
@@ -24,6 +25,7 @@ const Home = () => {
     }, []);
 
     return(
+      <>
         <div className="container home-page">
             <div className="text-zone">
                 <h1>
@@ -48,8 +50,10 @@ const Home = () => {
                 <h2>Full Stack Developer / Digital Marketer / Musician</h2>
                 <Link to ="/contact" className="flat-button">CONTACT ME</Link>
             </div>
-          
+            
         </div>
+        <Loader type="pacman" />
+      </>
     );
 }
 
